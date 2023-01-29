9490693825f25667531e4d5a64bb703b1e44bd6f
Samuel Ajayi <61334443+samajayi13@users.noreply.github.com>
2023-01-29 23:53:29 +0000

    Created BracteriaSolution and it's inital fields

---
+++ BacteriaSolution.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class BacteriaSolution {
+   private static PetriGridDish pretriDish = new PetriGridDish();
+   private static InputProcessor inputProcessor = new InputProcessor();
+   private static Scanner scanner  = new Scanner(System.in);
+
+    public static void main(String[] args) {
+       beginSimulation();
+   }
+
+}
