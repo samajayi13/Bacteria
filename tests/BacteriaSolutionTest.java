e6928d747cbe633f954df643f239031e6c05ab35
Samuel Ajayi <61334443+samajayi13@users.noreply.github.com>
2023-01-30 00:05:02 +0000

    Added test case for finding surviving cells

--- tests/BacteriaSolutionTest.java
+++ tests/BacteriaSolutionTest.java
@@ -9,4 +9,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BacteriaSolutionTest {
     BacteriaSolution simulation = new BacteriaSolution();
+    @Test
+    public void givenLiveCellsFindSurvingCells(){
+        List<String> livingCells = new ArrayList<>(Arrays.asList("1,2","2,2","3,2"));
+        System.out.println("living cells" + livingCells);
+        List<String> expected = new ArrayList<>(Arrays.asList("2,1","2,2","2,3"));
+        List<String> actual = simulation.findSurvivingCells(livingCells);
+        assertEquals(expected,actual);
+    }
 }
