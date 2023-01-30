2132c8e039f4e284e778f61621d4834d24dd9629
Samuel Ajayi <61334443+samajayi13@users.noreply.github.com>
2023-01-30 02:05:16 +0000

    updated test class for PetriGridDish

--- tests/PetriGridDishTest.java
+++ tests/PetriGridDishTest.java
@@ -3,4 +3,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PetriGridDishTest {
+    @Test
+    public void insertBacteria(){
+        int[][] expected = getMockDish();
+
+        PetriGridDish dish = new PetriGridDish();
+        dish.insertBacteria(1,2);
+        dish.insertBacteria(2,2);
+        dish.insertBacteria(3,2);
+        int[][]actual = dish.grid;
+        
+        assertArrayEquals(expected,actual);
+    }
+
+    private int[][] getMockDish(){
+        return new int[][] {
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    }
 }
