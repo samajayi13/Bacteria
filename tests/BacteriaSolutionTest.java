81d1d5e4277a434e4ba57cc549283483e944e691
Samuel Ajayi <61334443+samajayi13@users.noreply.github.com>
2023-01-30 00:41:35 +0000

    Added testcases to validate cell neighbours

--- tests/BacteriaSolutionTest.java
+++ tests/BacteriaSolutionTest.java
@@ -11,28 +11,59 @@ class BacteriaSolutionTest {
     BacteriaSolution simulation = new BacteriaSolution();
     @Test
     public void givenLiveCellsFindSurvingCells(){
+        simulation.pretriDish.grid = getMockDish();
         List<String> livingCells = new ArrayList<>(Arrays.asList("1,2","2,2","3,2"));
         System.out.println("living cells" + livingCells);
         List<String> expected = new ArrayList<>(Arrays.asList("2,1","2,2","2,3"));
         List<String> actual = simulation.findSurvivingCells(livingCells);
         assertEquals(expected,actual);
     }
-    
+
     @Test
     public void givenCellGetNumberOfLivingNeighbors(){
-        simulation.pretriDish.grid = new int[][] {  {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                                                    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                                                    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+        simulation.pretriDish.grid = getMockDish();
         int expected = 1;
         int actual = simulation.getNumberOfLivingNeighbors(2,1);
 
         assertEquals(expected,actual);
     }
+
+    @Test
+    public void givenNeighbourPositionCheckIfValid(){
+        boolean expected = true;
+        boolean actual =  simulation.isPositionValid(1,2);
+        assertEquals(expected,actual);
+    }
+
+
+    @Test
+    public void givenNeighbourPositionCheckIfInValid(){
+        boolean expected = true;
+        boolean actual =  simulation.isPositionValid(-1,11);
+        assertNotEquals(expected,actual);
+    }
+
+    @Test
+
+    public void givenNeighbourCheckIfAlive(){
+        simulation.pretriDish.grid = getMockDish();
+        boolean expected = true;
+        boolean actual = simulation.isNeighborAlive(0,1);
+        assertEquals(expected,actual);
+    }
+
+
+    private int[][] getMockDish(){
+        return new int[][] {  {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    }
+
 }
