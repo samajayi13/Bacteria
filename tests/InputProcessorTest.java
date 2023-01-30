84ef901a4fc4856e6878fe179ab38422f17bebb4
Samuel Ajayi <61334443+samajayi13@users.noreply.github.com>
2023-01-30 00:02:39 +0000

    Added test cases for getting row and column from user

--- tests/InputProcessorTest.java
+++ tests/InputProcessorTest.java
@@ -3,4 +3,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputProcessorTest {
+    @Test
+    public void Get_Column_From_Input(){
+        InputProcessor inputProcessor = new InputProcessor();
+        int expected = 2;
+        int actual = inputProcessor.getColumn("1,2");
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void Get_Row_From_Input(){
+        InputProcessor inputProcessor = new InputProcessor();
+        int expected = 1;
+        int actual = inputProcessor.getRow("1,2");
+
+        assertEquals(expected,actual);
+    }
 }
