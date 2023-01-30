01b94308763f0c0782e88d4457b400500e042527
Samuel Ajayi <61334443+samajayi13@users.noreply.github.com>
2023-01-30 02:06:48 +0000

    Cleaning PetriGridDish class

--- PetriGridDish.java
+++ PetriGridDish.java
@@ -2,8 +2,13 @@ public class PetriGridDish {
     public static final int ROWS  = 10;
     public static final int COLUMNS = 10;
 
+    //Below two fields is used to find all possible 8 neighbours a cell can have
     public static int[] neighborsX = {-1,-1,-1,0,0,1,1,1};
     public static int[] neighborsY =  {-1, 0, 1, -1, 1, -1, 0, 1};
 
     public static int[][] grid = new int[ROWS][COLUMNS];
+
+    public void insertBacteria(int row,int column){
+        this.grid[row - 1][column - 1] = 1;
+    }
 }
